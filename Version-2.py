ebace75b5e1bf30cc04bf44bbedee248330b6d66
zaidenhughes3-a11y <zaidenhughes3@gmail.com>
2025-10-30 13:07:55 -0400

    Create Version-2.py

---
+++ Version-2.py
@@ -0,0 +1,11 @@
+import random as rd
+
+magic_number = rd.randint(1,10)
+#print(magic_number)
+guess = int(input("Guess a number between 1 and 10: ") 
+
+while guess != magic_number:
+   print("You lose!")
+   guess = int(input("Guess a number between 1 and 10:"
+else:
+  print("You win!")
