afcf11b73417513298ca24b64add6915205bc264
zaidenhughes3-a11y <zaidenhughes3@gmail.com>
2025-11-04 10:44:06 -0500

    Update Version-6y.py

--- Version-6y.py
+++ Version-6y.py
@@ -1,1 +1,14 @@
+play = True
+couter = 5 
 
+while play:
+  while counter > 0:
+    print (counter)
+    counter -=1
+
+play = input ("Would yoou like to play again? (y/n): ")
+if play == "":
+  play = True
+  counter = 5
+else:
+  play = False
