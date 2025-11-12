9f66f842d93fcea10906c10a70a994a9e7058402
zaidenhughes3-a11y <zaidenhughes3@gmail.com>
2025-11-12 12:15:18 -0500

    Update Version-3.py

--- Version-3.py
+++ Version-3.py
@@ -1,1 +1,17 @@
+import random as rd
 
+magic_number = rd. randint(1,10)
+# print (magic_number)
+
+guess = int(input("Enter a number between 1 and 10: "))
+counter = 3
+
+while guess != magic_number and counter > 0:
+  counter -= 1
+  print("you lose!")
+  print ("%s guesses remain. " %(counter))
+  print("")
+  if counter == 0:
+    break
+  else:
+    guess = int(input("Guess a number beween1 and 10"))
