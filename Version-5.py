846034140c27b1fd71e4491faa75ef73f33aef7d
zaidenhughes3-a11y <zaidenhughes3@gmail.com>
2025-11-13 11:53:17 -0500

    Update Version-5.py

--- Version-5.py
+++ Version-5.py
@@ -1,1 +1,22 @@
-
+play-True
+while play:
+import random as rd numbers-ll
+magic_number= rd. randint (1,100).
+guess-int (input ("what is your guess?"))
+• counter-6
+while guess! magic_number and counter > counter-=1 numbers-append (guess)
+print"your already guessed %s "% (counter)) if magic_number-guess:
+print ("try lower number")
+if magic_number>guess:
+print("try a higher number")
+print ("you have d gueses, left"% (counter)) if counter==0:
+print("gameover")
+print (would you like th play another round")
+response=input ("y/n")
+if response == "y":
+attempt=1
+print ("okay round"+str(attempt))
+else:
+guessint(input"tryagain:"))
+if guess=-magic_number:
+print ("you win!")
